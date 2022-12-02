143847f337a0e35f58621ed2924154216bc87f12
Vikash Saxena <90413732+we-kash@users.noreply.github.com>
2022-12-02 15:32:14 +0530

    express package
    for using express package in our backend

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const app=express();
+const appe=express();
 const path=require('path');
-
+appe.use('/product',res);
 app.use('/',(req,res,next)=>{
     res.sendFile(path.join(__dirname,"success.html"));
 })
