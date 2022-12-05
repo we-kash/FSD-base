7183d6fbe4e8fbe099eb238c6ce8ae32f67f913d
Vikash Saxena <90413732+we-kash@users.noreply.github.com>
2022-12-05 22:24:28 +0530

    backend created
    added knex database module

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -15,7 +15,7 @@ const db = knex({
 
 const app = express();
 
-let intialPath = path.join(__dirname, "public");
+let intialPath = path.join(__dirname, "..");
 
 app.use(bodyParser.json());
 app.use(express.static(intialPath));
@@ -32,3 +32,47 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(intialPath, "register.html"));
 })
 
+app.post('/register-user', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if(!name.length || !email.length || !password.length){
+        res.json('fill all the fields');
+    } else{
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if(err.detail.includes('already exists')){
+                res.json('email already exists');
+            }
+        })
+    }
+})
+
+app.post('/login-user', (req, res) => {
+    const { email, password } = req.body;
+
+    db.select('name', 'email')
+    .from('users')
+    .where({
+        email: email,
+        password: password
+    })
+    .then(data => {
+        if(data.length){
+            res.json(data[0]);
+        } else{
+            res.json('email or password is incorrect');
+        }
+    })
+})
+
+app.listen(3000, (req, res) => {
+    console.log('listening on port 3000......')
+})
